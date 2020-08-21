8d807609d80069c8117216fef528ab78dc6195dd
Rob Herrera <rob.herrera@kount.com>
2020-08-21 16:21:56 -0600

    add some more

--- microservices/webserver/code/main.go
+++ microservices/webserver/code/main.go
@@ -3,9 +3,22 @@ package main
 import (
         "fmt"
         "net/http"
+        "log"
+    
+    	"github.com/go-redis/redis"
     )
     
     func main() {
+        fmt.Println("Go Redis Tutorial")
+
+	client := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		Password: "",
+		DB: 0,
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
         http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
             fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
         })
